953288d6d77ded8f8bd2228b02c904e26c9d3d47
stifler33 <bagi33.ru@gmail.com>
2021-04-18 16:50:32 +0300

    task 2 init

--- task2.cpp
+++ task2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include <vector>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<float> food_list = {2.39 , 5.5 , 299.0 , 25.99 , 49.0 , 69.49};
+    vector<int> shop_list = {0,3,4,5,1};
+    float sum = 0;
+    for (int i : shop_list){
+        sum += food_list[i];
+    }
+    cout << "sum " << sum << endl;
     return 0;
 }
