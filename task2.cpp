bd08b78370f55b409eea059236b7cbf8f7829576
stifler33 <bagi33.ru@gmail.com>
2021-04-18 16:57:45 +0300

    task 2 finish

--- task2.cpp
+++ task2.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 int main() {
     vector<float> food_list = {2.39 , 5.5 , 299.0 , 25.99 , 49.0 , 69.49};
-    vector<int> shop_list = {0,3,4,5,1};
+    vector<int> shop_list = {2,3,5,0,1};
     float sum = 0;
     for (int i : shop_list){
-        sum += food_list[i];
+        if (i < food_list.size()){
+            sum += food_list[i];
+        }else cout <<"'" << i << "' not correct index !\n";
     }
     cout << "sum " << sum << endl;
     return 0;
