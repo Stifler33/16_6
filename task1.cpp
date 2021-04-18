1c7c6ab21ff535a90711c921c84034d47ed9c976
stifler33 <bagi33.ru@gmail.com>
2021-04-18 14:36:43 +0300

    added check sumNumb

--- task1.cpp
+++ task1.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+int check (int count){
+    while (count <= 0){
+        cout << "number less or equal 0 !\n";
+        cin >> count;
+    }
+    return count;
+}
 int main() {
     vector<int> sequence;
-    int counter;
+    int sumNumb;
     cout << "Enter sum number ";
-    cin >> counter;
-    for (int i = 0; i < counter; i++){
+    cin >> sumNumb;
+    if (sumNumb == 0) sumNumb = check(sumNumb);
+    for (int i = 0; i < sumNumb; i++){
        int number;
        cout << "Enter number sequence\n";
        cin >> number;
