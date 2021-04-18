71a9a695e91e7dd95d00e4c982e9f486796ed42b
stifler33 <bagi33.ru@gmail.com>
2021-04-18 17:23:30 +0300

    task 3 finish

--- task3.cpp
+++ task3.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-
+#include <vector>
+using namespace std;
+const int sizeVec = 20;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> vec;
+    int number = 0;
+    cout << "Enter number\n";
+    while (number != -1){
+        cin >> number;
+        if (number == -1) continue;
+        if ((vec.size() + 1) > sizeVec){
+            vec.erase(vec.begin());
+            vec.push_back(number);
+        }else  vec.push_back(number);
+        cout << "vec.size " << vec.size() << endl;
+    }
+    for (int i : vec){
+        cout << i << " ";
+    }
     return 0;
 }
